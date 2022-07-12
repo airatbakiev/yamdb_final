409332cd1bdcc698ad2386573972d3eb40edf950
Airat Bakiev <bakievaa@mail.ru>
2022-07-13 00:29:24 +0500

    finaltask

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,6 +1,6 @@
 import os
-
 from datetime import timedelta
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -71,7 +71,7 @@ DATABASES = {
     'default': {
         'ENGINE': os.getenv(
             'DB_ENGINE', default='django.db.backends.postgresql'
-            ),
+        ),
         'NAME': os.getenv('DB_NAME', default='postgres'),
         'USER': os.getenv('POSTGRES_USER', default='postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
